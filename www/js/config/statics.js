cdef0872f5c6c738e5b97c6a4f23b7e5a18f4d41
zetahawke <michel.szinavel@gmail.com>
2016-07-12 22:40:57 -0400

    fix: fixing urls that must be in secure mode (https) for do a request for that resource from heroku

--- www/js/config/statics.js
+++ www/js/config/statics.js
@@ -6,7 +6,7 @@
 
 (function() {
   this.app.constant('ENV', {
-   'API_MARVEL_GET_SERVICES': 'http://gateway.marvel.com/v1/public/comics',
+   'API_MARVEL_GET_SERVICES': 'https://gateway.marvel.com/v1/public/comics',
    'API_PUBLIC_KEY': 'e3e6b4b3bdb64ee27062c59db0667fe1',
    'API_PRIVATE_KEY': '92403dab747a7ddf0a507d986d506f27e9ca533a',
   });
